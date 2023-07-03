1d38ef05f84b87f771a6b82d372de41c71346ae5
M4Nyak <119705619+M4Nyak@users.noreply.github.com>
2023-07-03 14:44:28 +0300

    Update hwidkill.py
    artık macde değişim kapsamında

--- hwidkill.py
+++ hwidkill.py
@@ -1,13 +1,16 @@
 import subprocess
 
 sahte = input('Sahte HWID\'inizi giriniz: ')
-
+sahte2= input('Sahte mac adresini giriniz: ')
 def hwid_degistir():
     # HWID değiştirmek için kullanicam komut ( 'wmic'  kullanacam)
     hwid_degistirme_komutu = f'wmic csproduct set uuid="{sahte}"'
+    mac_degistirme_komutu= f'wmic nicconfig where Description="Ethernet Bağlantısı" call setMACAddress="{sahte2}"
+
 
     try:
         subprocess.run(hwid_degistirme_komutu, shell=True, check=True)
+        subprocess.run(mac_degistirme_komutu, shell=True, check=True)
         print("HWID başarıyla değiştirildi.hiçbir engel beni zaptedemez")
     except subprocess.CalledProcessError:
         print("HWID değiştirilemedi. sen benim gibi gizli değilsin.")
